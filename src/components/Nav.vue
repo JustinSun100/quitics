f1124a0513cb9180fbcd99768b428ed24027fdc0
justinsun <sun_heqing@163.com>
2021-03-16 19:33:52 +0800

    完成了Nav全局组件

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <router-link to="/money">金钱</router-link>
+    <router-link to="labels">标签</router-link>
+    <router-link to="statistics">统计</router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
+
+@Component({
+  components: {Statistics, Labels, Money}
+})
+export default class Nav extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Found404.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  输入网址错误，请检查
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Found404 extends Vue{
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
